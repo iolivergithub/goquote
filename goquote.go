b74ca70385e27bf68cd5c37d86e6e737712560e9
Ian <30772529+iolivergithub@users.noreply.github.com>
2020-08-24 23:13:59 +0300

    Update goquote.go

--- goquote.go
+++ goquote.go
@@ -1,29 +1,60 @@
-package main  
- 
-import ( 
-	       "fmt"
-	       "flag"
-	       "os"
+package main
 
-	       "github.com/google/go-tpm/tpm2"
-       )
+import (
+    "flag"
+	"fmt"
+    "reflect"
+
+	"github.com/google/go-tpm/tpm2"
+	"github.com/google/go-tpm/tpmutil"
+)
+
 
 var (
-
-    )
+	tpmPath = flag.String("tpm-path", "/dev/tpm0", "Path to the TPM device (character device or a Unix socket)")
+)
 
 func main() {
 
-   var tpmname = flag.String("tpm","/dev/tpm0", "The path to the TPM device")
-   flag.Parse()
+	rwc, err := tpm2.OpenTPM(*tpmPath)
+	if err != nil {
+		fmt.Errorf("can't open TPM at  %v", err)
+	}
+	//defer rwc.Close()
 
-   rwc,err := tpm2.OpenTPM(*tpmname)
-   if err != nil {
-          fmt.Fprintf(os.Stderr,"Opening TPM device failed: ",err)
-          os.Exit(1)
-      }
+        nonce := []byte{1, 2, 3, 4, 5, 6, 7, 8}
 
-   defer rwc.Close()
+        var handle = tpmutil.Handle(0x810100AA)
 
+        fmt.Println("Handle is",handle)
+    	fmt.Println("Type:",reflect.TypeOf(handle).String())
+
+        // quote retuns 3 values,  att, sig, err
+        att, sig, err := tpm2.Quote(
+                 rwc,
+                 handle,
+ 		"",
+		"",
+		nonce,
+                tpm2.PCRSelection{ tpm2.AlgSHA256 , []int{0} },
+		tpm2.AlgNull)
+
+	if err != nil {
+		fmt.Errorf("Problem getting quote  %s", err)
+                fmt.Println(err)
+	}
+	if sig != nil {
+		fmt.Errorf("Sig is nil\n")
+        }
+	if att != nil {
+		fmt.Errorf("Att is nil\n")
+        }
+        // att is of type []byte
+        // sig is of type tpm2.Signature ??
+        // err is ??
+        fmt.Println("Err is *",err,"*")
+        fmt.Println("length  ",len(att))
+        fmt.Println("Att [% x] ",att)
+    
 
 }
